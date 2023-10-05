48f5ddc63d0815bda9e69d3ea0340a0ffb91600f
JodhiParson <jodhiparson@gmail.com>
2023-10-05 01:37:17 -0400

    refactor: Merge shrug, tableflip, and unflip into TextCog

--- cogs/textCog.py
+++ cogs/textCog.py
@@ -1,18 +1,32 @@
 import discord
+
 from discord.ext import commands
 
-class Unflip(commands.Cog):
+class TextCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
     @commands.Cog.listener() #.event
     async def on_ready(self):
-        print("Unflip.py is ready!")
+        print("Text.py is ready!")
+        
+    @commands.command()
+    #shrug
+    async def shrug(self, ctx):
+        
+        await ctx.send(f"¯\_(ツ)_/¯")
+        
+    @commands.command()
+    #tableflip
+    async def tableflip(self, ctx):
+        
+        await ctx.send(f"(╯°□°）╯︵ ┻━┻")
     
     @commands.command() #.command
     async def unflip(self, ctx): # note: self goes first before any other parameter
         
         await ctx.send(f"┬─┬ ノ( ゜-゜ノ)")
         
+    
 async def setup(bot):
-    await bot.add_cog(Unflip(bot))
+    await bot.add_cog(TextCog(bot))
