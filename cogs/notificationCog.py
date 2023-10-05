65f225b9b1a905ed2c11f7433b3e87e8e76dec0c
JodhiParson <jodhiparson@gmail.com>
2023-10-05 01:20:37 -0400

    Add set_notification_channel command to NotificationCog
    This commit introduces a new command, set_notification_channel, to the NotificationCog. The command allows users to set the Discord channel where notifications will be sent for subscribed channels. If no channel name is provided, the current channel is used.
    
    Command usage: =set_notification_channel [channel_name]

---
+++ cogs/notificationCog.py
@@ -0,0 +1,49 @@
+import json
+import discord
+from discord.ext import commands
+
+class NotificationCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("NotificationCog.py is ready!")
+
+    @commands.command()
+    async def set_notification_channel(self, ctx, *, channel_name=None):
+        if channel_name is None:
+            # If no channel name is provided, set the notifying Discord channel to the current channel
+            channel_id = ctx.channel.id
+        else:
+            # Remove leading and trailing whitespaces and check if the name starts and ends with quotes
+            if channel_name.startswith('"') and channel_name.endswith('"'):
+                # Remove the quotes and leading/trailing whitespaces
+                channel_name = channel_name[1:-1].strip()
+
+            # Find the channel by name
+            channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+
+            if channel is None:
+                await ctx.send(f"Channel '{channel_name}' not found.")
+                return
+
+            channel_id = channel.id
+
+        # Load existing data from the JSON file
+        with open("youtubedata.json", "r") as f:
+            data = json.load(f)
+
+        # Update the notifying Discord channel for all subscribed channels in the current guild
+        for channel_data in data.values():
+            if channel_data["notifying_discord_channel"] != channel_id:
+                channel_data["notifying_discord_channel"] = channel_id
+
+        # Save the updated data back to the JSON file
+        with open("youtubedata.json", "w") as f:
+            json.dump(data, f)
+
+        await ctx.send(f"Notifying Discord channel set to {ctx.channel.mention}")
+
+async def setup(bot):
+    await bot.add_cog(NotificationCog(bot))
