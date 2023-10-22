03f9d50ab1b8769ebe58d4c399d5e07e34634a5e
JodhiParson <jodhiparson@gmail.com>
2023-10-21 21:59:40 -0400

    Added function to retrieve channel's profile pic

--- cogs/embed.py
+++ cogs/embed.py
@@ -1,5 +1,11 @@
 import discord
 from discord.ext import commands
+import json
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 class Embed(commands.Cog):
     def __init__(self, bot):
@@ -11,15 +17,40 @@ class Embed(commands.Cog):
     
     @commands.command() #.command
     async def embed(self, ctx): # note: self goes first before any other parameter
-        embed_message = discord.Embed(title="Welcome to BodhiBot",description="SUISEX", color=discord.Color.red())
+        # Load data from the JSON file
+        with open("youtubedata.json", "r") as f:
+            data = json.load(f)
+            
+        embed_message = discord.Embed(title="Subscribed Channels", color=discord.Color.red())
         
         embed_message.set_author(name=f"Requested by @{ctx.author.display_name}", icon_url =ctx.author.avatar)
-        embed_message.set_thumbnail(url=ctx.guild.icon)
-        embed_message.set_image(url=ctx.guild.icon)
-        embed_message.add_field(name="Field name", value="field value", inline=False)
+        embed_message.set_thumbnail(url=ctx.author.avatar)
+        #embed_message.set_image(url=ctx.guild.icon)
+        for channel_id, channel_data in data.items():
+            profile_picture_url = self.get_channel_profile_picture(channel_id)
+            embed_message.add_field(
+                name=channel_data['channel_name'],
+                value=f"Channel ID: {channel_id}",
+                inline=False
+            )
+        embed_message.set_thumbnail(url=profile_picture_url)
         embed_message.set_footer(text=f"@{ctx.author.display_name}", icon_url=ctx.author.avatar)
         
         await ctx.send(embed = embed_message)
+        
+    def get_channel_profile_picture(self, channel_id):
+        # Use your YouTube Data API key here
+        api_key = os.getenv('YOUTUBE_API_KEY')
+
+        # Make a request to the YouTube Data API
+        url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet&id={channel_id}&key={api_key}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            if 'items' in data and len(data['items']) > 0:
+                return data['items'][0]['snippet']['thumbnails']['default']['url']
+        return "No Image Found"  # Default image if the profile picture is not available
         
 async def setup(bot):
     await bot.add_cog(Embed(bot))
