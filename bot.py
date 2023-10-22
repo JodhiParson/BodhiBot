9b80c15c96fb7090dba36bd3905173dfeb5f6125
JodhiParson <jodhiparson@gmail.com>
2023-10-21 21:56:12 -0400

    Fixed Bug w/ Repeating Videos and Streams

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ async def on_ready():
         """
     change_status.start()
     checkforvideos.start()
+    checkforstreams.start()
 
 #note: 'async' functions ALWAYS need an 'await' somewhere in the function
 async def load():
@@ -56,10 +57,11 @@ async def on_message(message):
 #checking for vidoes every 30 seconds
 #you can check for vidoes every 10 seconds also but i would prefer to keep 30 seconds
 @tasks.loop(seconds=30)
+
 async def checkforvideos():
   with open("youtubedata.json", "r") as f:
     data=json.load(f) # loads the data in the json
-  
+  #print("Now Checking For Videos!")
   #printing here to show
   #print("Now Checking!")
 
@@ -94,13 +96,61 @@ async def checkforvideos():
 
       #sending the msg in discord channel
       #you can mention any role like this if you want
-        msg = f"@everyone {data[str(youtube_channel)]['channel_name']} Just Uploaded A Video or they are Live Go Check It Out: {latest_video_url}"
+        videoMsg = f"{data[str(youtube_channel)]['channel_name']} Just Uploaded A Video! : {latest_video_url}"
       #if you'll send the url discord will automaitacly create embed for it
       #if you don't want to send embed for it then do <{latest_video_url}>
 
-        await discord_channel.send(msg)
+        await discord_channel.send(videoMsg)
+#checking for vidoes every 30 seconds
+#you can check for vidoes every 10 seconds also but i would prefer to keep 30 seconds
+@tasks.loop(seconds=20)
+async def checkforstreams():
+    with open("youtubedata.json", "r") as f:
+        data = json.load(f)
 
+    #print("Now Checking For Streams!")
 
+    for youtube_channel in data:
+        channel = f"https://www.youtube.com/channel/{youtube_channel}"
+
+        try:
+            livehtml = requests.get(channel + "/streams").text
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 404:
+                print(f"The channel {data[youtube_channel]['channel_name']} does not have a /streams section.")
+                continue
+            else:
+                print(f"An error occurred for {data[youtube_channel]['channel_name']}: {e}")
+                continue
+        except Exception as e:
+            print(f"An error occurred for {data[youtube_channel]['channel_name']}: {e}")
+            continue
+
+        try:
+            latest_video_url2 = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', livehtml).group()
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 404:
+                print(f"The channel {data[youtube_channel]['channel_name']} does not have a /streams section.")
+                continue
+            else:
+                print(f"An error occurred for {data[youtube_channel]['channel_name']}: {e}")
+                continue
+        except Exception as e:
+            print(f"An error occurred for {data[youtube_channel]['channel_name']}: {e}")
+            continue
+
+        if 'latest_stream_url' in data[youtube_channel]:
+            if not str(data[youtube_channel]['latest_stream_url']) == latest_video_url2:
+              if data[youtube_channel]['latest_video_url'] != latest_video_url2:
+                print("A stream has been uploaded!")
+                data[youtube_channel]['latest_stream_url'] = latest_video_url2
+                with open("youtubedata.json", "w") as f:
+                    json.dump(data, f)
+                discord_channel_id = data[youtube_channel]['notifying_discord_channel']
+                discord_channel = bot.get_channel(int(discord_channel_id))
+                liveMsg = f"{data[youtube_channel]['channel_name']} Just Uploaded A Stream: {latest_video_url2}"
+
+                await discord_channel.send(liveMsg)
 
 
 
