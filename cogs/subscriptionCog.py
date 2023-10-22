a75578cb8d555be24036fbbf1e84e030c36ffa69
JodhiParson <jodhiparson@gmail.com>
2023-10-21 22:02:01 -0400

    Edited subscribe command to include stream posts Added command to rename subscribed channels

--- cogs/subscriptionCog.py
+++ cogs/subscriptionCog.py
@@ -44,16 +44,26 @@ class SubscriptionCog(commands.Cog):
         await ctx.send(message)
         
     @commands.command(aliases=["follow"])
-    async def subscribe(self, ctx, channel_id, *, channel_name):
+    async def subscribe(self, ctx, channel_id = None, *, channel_name = None):
+        if channel_id is None or channel_name is None:
+            await ctx.send("Usage: `=subscribe channel_id channel_name`")
+        else: 
         # Check if the channel is already subscribed
-        with open("youtubedata.json", "r") as f:
-            data = json.load(f)
+            with open("youtubedata.json", "r") as f:
+                data = json.load(f) 
+               
+        # Add the new channel data
+            data[channel_id] = {
+        "channel_name": channel_name,
+        "latest_video_url": "",  # Initialize with empty URL
+        "latest_stream_url": "",  # Initialize with empty URL
+        "notifying_discord_channel": ctx.channel.id #set to ID of channel where command was used
+    }
+    # Save the updated data back to youtubedata.json
+        with open("youtubedata.json", "w") as f:
+            json.dump(data, f, indent=4) 
+            
 
-        if channel_id in data:
-            await ctx.send("You are already subscribed to this channel.")
-        else:
-            # You can replace 'YourDiscordChannelID' with the actual channel ID where you want to receive notifications
-            self.add_channel(channel_id, channel_name, ctx.channel.id)
             await ctx.send(f"Subscribed to the channel with ID: {channel_id} and name: {channel_name}")
 
     @commands.command(aliases=["unfollow"])
@@ -81,6 +91,31 @@ class SubscriptionCog(commands.Cog):
         # Save the updated data back to the JSON file
         with open("youtubedata.json", "w") as f:
             json.dump(data, f)
+            
+    @commands.command(aliases=["rename_channel"])
+    async def rename(self, ctx, channel_name, new_channel_name):
+        # Check if the user has permission to manage channels
+        if not ctx.author.guild_permissions.manage_channels:
+            await ctx.send("You don't have the permission to manage channels.")
+            return
 
+        # Load existing data from the JSON file
+        with open("youtubedata.json", "r") as f:
+            data = json.load(f)
+
+        # Check if the channel with the given name exists
+        for channel_id, channel_data in data.items():
+            if channel_data["channel_name"].lower() == channel_name.lower():
+                # Rename the channel in the JSON data
+                channel_data["channel_name"] = new_channel_name
+
+                # Save the updated data back to the JSON file
+                with open("youtubedata.json", "w") as f:
+                    json.dump(data, f)
+
+                await ctx.send(f"Channel '{channel_name}' renamed to '{new_channel_name}'.")
+                return
+
+        await ctx.send(f"Channel '{channel_name}' not found in your subscriptions.")
 async def setup(bot):
     await bot.add_cog(SubscriptionCog(bot))
